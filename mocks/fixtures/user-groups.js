2c302be301f9c7484bfece10c2a2e2776386c652
Ion Cretu <icretu@haventech.us>
2024-11-13 15:47:44 +0200

    add base mock-server;

---
+++ mocks/fixtures/user-groups.js
@@ -0,0 +1,200 @@
+module.exports = [
+	{
+		id: "1",
+		externalGroupId: "1",
+		name: "Account manager",
+		description:
+			"Account manager are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-02-02"
+	},
+	{
+		id: "2",
+		externalGroupId: "2",
+		name: "Business system administrator",
+		description:
+			"Business system administrator are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-02-02"
+	},
+	{
+		id: "3",
+		externalGroupId: "3",
+		name: "Auditor",
+		description:
+			"Auditors are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-02-02"
+	},
+	{
+		id: "4",
+		externalGroupId: "4",
+		name: "Case manager",
+		description:
+			"Case managers are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "5",
+		externalGroupId: "5",
+		name: "Delivery / 1035",
+		description:
+			"Delivery / 1035 are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "6",
+		externalGroupId: "6",
+		name: "Info Specialist",
+		description:
+			"Info Specialist are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "7",
+		externalGroupId: "7",
+		name: "IPM team",
+		description:
+			"IPM team are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "8",
+		externalGroupId: "8",
+		name: "Life BA team",
+		description:
+			"Life BA team are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "9",
+		externalGroupId: "9",
+		name: "New business operations support",
+		description:
+			"New business operations support are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "10",
+		externalGroupId: "10",
+		name: "New business view only",
+		description:
+			"New business view only are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "11",
+		externalGroupId: "11",
+		name: "NonUnderwriting directors",
+		description:
+			"NonUnderwriting directors are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "12",
+		externalGroupId: "12",
+		name: "PHI team",
+		description:
+			"PHI team are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-01-01",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "13",
+		externalGroupId: "13",
+		name: "Manager",
+		description:
+			"Managers are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-02-02",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "14",
+		externalGroupId: "14",
+		name: "Relationship manager",
+		description:
+			"Relationship manager are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-02-02",
+		updatedAt: "2024-03-03"
+	},
+	{
+		id: "15",
+		externalGroupId: "15",
+		name: "Medical officer",
+		description:
+			"Medical officers are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-02-02",
+		updatedAt: "2024-04-04"
+	},
+	{
+		id: "16",
+		externalGroupId: "16",
+		name: "Underwriter",
+		description:
+			"Underwriters are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-04-04",
+		updatedAt: "2024-06-06"
+	},
+	{
+		id: "17",
+		externalGroupId: "17",
+		name: "Underwriter auditor",
+		description:
+			"Underwriter auditor are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "active",
+		createdAt: "2024-04-04",
+		updatedAt: "2024-06-06"
+	},
+	{
+		id: "18",
+		externalGroupId: "18",
+		name: "Underwriting directors",
+		description:
+			"Underwriting directors are responsible for evaluating risk and determining the underwriting decision for cases that are assigned to them.",
+		tenantId: "abcdef-abcdef-abcdef-abcdef",
+		status: "inactive",
+		createdAt: "2024-04-04",
+		updatedAt: "2024-06-06"
+	}
+]
---
+++ mocks/routes/user-groups.js
@@ -0,0 +1,172 @@
+const userGroups = require("../fixtures/user-groups")
+
+const apiMutation = {
+	$metadata: {
+		httpStatusCode: 200,
+		requestId: "071a4076-0550-46f6-829f-9cbe83c057b0",
+		attempts: 1,
+		totalRetryDelay: 0
+	},
+	Entries: [
+		{
+			EventId: "c950f268-2f3d-e49d-636a-ffa149b61be9"
+		}
+	],
+	FailedEntryCount: 0
+}
+
+module.exports = [
+	{
+		id: "get-user-groups-paginated", // id of the route
+		url: "/admin/api/identity-management-svc/control/external/get-user-groups", // url in path-to-regexp format
+		method: "POST", // HTTP method
+		variants: [
+			{
+				id: "success", // id of the variant
+				type: "middleware", // variant type
+				options: {
+					middleware: (req, res, next, core) => {
+						core.logger.info("Request received!")
+						const body = req.body
+						const { pageNumber, pageSize } = body
+
+						const todoRes = {
+							rows: userGroups.slice((pageNumber - 1) * pageSize, pageNumber * pageSize),
+							pageNumber,
+							pageSize,
+							total: userGroups.length
+						}
+
+						res.status(200)
+						res.send(todoRes)
+					}
+				}
+			},
+			{
+				id: "empty", // id of the variant
+				type: "json", // variant type
+				options: {
+					status: 200,
+					body: {
+						rows: [],
+						pageNumber: 1,
+						pageSize: 10,
+						total: 0
+					}
+				}
+			}
+		]
+	},
+	{
+		id: "get-user-group", // id of the route
+		url: "/admin/api/identity-management-svc/control/external/user-groups/get-user-groups-by-ids", // url in path-to-regexp format
+		method: "POST", // HTTP method
+		variants: [
+			{
+				id: "success", // id of the variant
+				type: "middleware", // variant type
+				options: {
+					middleware: (req, res, next, core) => {
+						core.logger.info("Request received!")
+						const body = req.body
+						const { userGroupIds } = body
+
+						const todoRes = [userGroups.find((userGroup) => userGroupIds.includes(userGroup.id))]
+
+						res.status(200)
+						res.send(todoRes)
+					}
+				}
+			},
+			{
+				id: "not-found", // id of the variant
+				type: "json", // variant type
+				options: {
+					status: 404
+				}
+			}
+		]
+	},
+	{
+		id: "create-user-groups", // id of the route
+		url: "/admin/api/identity-management-svc/control/external/user-groups/create", // url in path-to-regexp format
+		method: "POST", // HTTP method
+		variants: [
+			{
+				id: "success", // id of the variant
+				type: "middleware", // variant type
+				options: {
+					middleware: (req, res, next, core) => {
+						core.logger.info("Request received!")
+						const createUserGroup = req.body
+
+						if (createUserGroup) {
+							userGroups.push({ ...createUserGroup, id: Math.max(...userGroups.map((a) => parseInt(a.id))) + 1 })
+							res.status(200)
+							res.send(apiMutation)
+						} else {
+							res.status(404)
+							res.send()
+						}
+					}
+				}
+			}
+		]
+	},
+	{
+		id: "update-user-groups", // id of the route
+		url: "/admin/api/identity-management-svc/control/external/user-groups/update/:id", // url in path-to-regexp format
+		method: "POST", // HTTP method
+		variants: [
+			{
+				id: "success", // id of the variant
+				type: "middleware", // variant type
+				options: {
+					middleware: (req, res, next, core) => {
+						core.logger.info("Request received!")
+						const updateUserGroup = req.body
+						const userGroupId = req.params.id
+
+						const userGroupIndex = userGroups.findIndex((user) => user.id === userGroupId)
+
+						if (updateUserGroup && userGroupId && userGroupIndex >= 0) {
+							userGroups[userGroupIndex] = { ...userGroups[userGroupIndex], ...updateUserGroup }
+							res.status(200)
+							res.send(apiMutation)
+						} else {
+							res.status(404)
+							res.send()
+						}
+					}
+				}
+			}
+		]
+	},
+	{
+		id: "delete-user-groups", // id of the route
+		url: "/admin/api/identity-management-svc/control/external/user-groups/delete/:id", // url in path-to-regexp format
+		method: "POST", // HTTP method
+		variants: [
+			{
+				id: "success", // id of the variant
+				type: "middleware", // variant type
+				options: {
+					middleware: (req, res, next, core) => {
+						core.logger.info("Request received!")
+						const { id } = req.params
+
+						const userGroupIndex = userGroups.findIndex((userGroup) => userGroup.id === id)
+						if (userGroupIndex >= 0) {
+							userGroups.splice(userGroupIndex, 1)
+							res.status(200)
+							res.send(apiMutation)
+						} else {
+							res.status(404)
+							res.send()
+						}
+					}
+				}
+			}
+		]
+	}
+]
